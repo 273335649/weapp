46c996e215ec2dd549e2f4fd444560ff20e3fc49
273335649 <160387337+273335649@users.noreply.github.com>
2025-01-03 20:53:51 +0800

    feat: 新增人脸识别

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,11 +1,8 @@
-import { Component, PropsWithChildren, useEffect } from 'react'
-import { connect } from 'react-redux'
+import { Component, PropsWithChildren, useEffect, useRef, useState } from 'react'
 import { View, Button, Text } from '@tarojs/components'
 import CameraPage from './components/VideoRecorder'
-import { add, minus, asyncAdd } from '../../actions/counter'
-
+import Taro, { CameraFrameListener } from '@tarojs/taro'
 import './index.less'
-import Taro from '@tarojs/taro'
 
 
 // #region 书写注意
@@ -40,78 +37,79 @@ interface Index {
   props: IProps;
 }
 
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add() {
-    dispatch(add())
-  },
-  dec() {
-    dispatch(minus())
-  },
-  asyncAdd() {
-    dispatch(asyncAdd())
-  }
-}))
-class Index extends Component<PropsWithChildren> {
-  componentWillReceiveProps(nextProps) {
-    console.log(this.props, nextProps)
-  }
-
-  componentWillUnmount() { }
-
-  componentDidShow() { }
-
-  componentDidHide() { }
-
-  componentDidMount(): void {
-    Taro.request({
-      url: '/api/environment/river',
-      data: {
-        key: 'b6f0832fc64a4a8790225aa612864da5',
-        page: 20
-      },
-      success(result) {
-        console.log('====================================');
-        console.log(result);
-        console.log('====================================');
-      },
+function Index(props: IProps) {
+  const listen = useRef<CameraFrameListener | null>(null);
+  const [num, setNum] = useState(0);
+  function recordVideo() {
+    return Taro.chooseVideo({
+      sourceType: ['camera'], // 指定使用摄像头录制
+      // camera: 'back', // 指定使用后置摄像头，可选值有front、back
+      maxDuration: 60, // 最大录制时长（秒）
+      camera: 'front', // 录制视频使用前置或后置摄像头
+    }).then(res => {
+      // 获取视频录制完成后的临时文件路径
+      const tempFilePath = res.tempFilePath
+      console.log('录制的视频文件路径:', tempFilePath);
+      // 处理视频文件，例如上传到服务器或本地保存等
+    }).catch(err => {
+      // 处理错误情况
+      console.error('录制视频失败:', err)
     })
   }
 
-  render() {
-    function recordVideo() {
-      return Taro.chooseVideo({
-        sourceType: ['camera'], // 指定使用摄像头录制
-        // camera: 'back', // 指定使用后置摄像头，可选值有front、back
-        maxDuration: 60, // 最大录制时长（秒）
-        camera: 'front', // 录制视频使用前置或后置摄像头
-      }).then(res => {
-        // 获取视频录制完成后的临时文件路径
-        const tempFilePath = res.tempFilePath
-        console.log('录制的视频文件路径:', tempFilePath);
-        // 处理视频文件，例如上传到服务器或本地保存等
-      }).catch(err => {
-        // 处理错误情况
-        console.error('录制视频失败:', err)
-      })
+  const callBack = (res: Taro.faceDetect.SuccessCallbackOption, frame: Taro.CameraContext.OnCameraFrameCallbackResult) => {
+    console.log(res);
+    if (res.confArray.global < 0.99) {
+      setNum(1);
+    }else{
+      setNum(0);
     }
-
-    return (
-      <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World2</Text></View>
-        <View><Text>录制视频</Text></View>
-        <CameraPage />
-        <Button className='startRecord' onClick={() => {
-          recordVideo();
-        }}>RecordVideo</Button>
-      </View>
-    )
   }
+
+  const startFace = () => {
+    const cameraContext = Taro.createCameraContext();
+    Taro.initFaceDetect({
+      success: () => {
+        listen.current = cameraContext.onCameraFrame((frame) => {
+          Taro.faceDetect({
+            frameBuffer: frame.data,
+            width: frame.width,
+            height: frame.height,
+            enablePoint: true,
+            enableConf: true,
+            success(res) {
+              callBack(res, frame);
+            },
+          })
+        })
+        listen.current.start();
+      },
+      fail: (res) => {
+        console.log(res);
+      }
+    })
+
+  }
+
+  return (
+    <View className='index'>
+      <View><Text>Hello, World2</Text></View>
+      <View><Text>录制视频</Text></View>
+      <CameraPage />
+      <Button type={num === 0 ? 'primary' : num === 1 ? 'warn' : 'default'}
+        onClick={() => {
+          listen.current?.stop();
+          Taro.stopFaceDetect({});
+        }}
+      >重置识别状态</Button>
+      <Button onClick={() => {
+        startFace();
+      }}>开始人脸识别</Button>
+      <Button className='startRecord' onClick={() => {
+        recordVideo();
+      }}>RecordVideo</Button>
+    </View>
+  )
 }
 
 export default Index
